874552f1a9bb685ba0cd766da7e2d12c49d1893d
Omar Soto <omarsotosainz@gmail.com>
2022-10-04 17:33:25 -0600

    first commit

---
+++ backend/class/Categorias.php
@@ -0,0 +1,78 @@
+<?php
+class Categorias{   
+    
+    private $itemsTable = "categorias";      
+    public $categoria_id;
+    public $nombre;
+    private $conn;
+	
+    public function __construct($db){
+        $this->conn = $db;
+    }	
+	
+	function read(){	
+		if($this->id) {
+			$stmt = $this->conn->prepare("SELECT * FROM ".$this->itemsTable." WHERE id = ?");
+			$stmt->bind_param("i", $this->id);					
+		} else {
+			$stmt = $this->conn->prepare("SELECT * FROM ".$this->itemsTable);		
+		}		
+		$stmt->execute();			
+		$result = $stmt->get_result();		
+		return $result;	
+	}
+	
+	function create(){
+		
+		$stmt = $this->conn->prepare("
+			INSERT INTO ".$this->itemsTable."(`nombre`)
+			VALUES(?)");
+		
+		$this->nombre = htmlspecialchars(strip_tags($this->nombre));
+		
+		$stmt->bind_param("ssiis", $this->nombre);
+		
+		if($stmt->execute()){
+			return true;
+		}
+	 
+		return false;		 
+	}
+		
+	function update(){
+	 
+		$stmt = $this->conn->prepare("
+			UPDATE ".$this->itemsTable." 
+			SET name= ? 
+			WHERE id = ?");
+	 
+		$this->id = htmlspecialchars(strip_tags($this->id));
+		$this->name = htmlspecialchars(strip_tags($this->name));
+	 
+		$stmt->bind_param("ssiisi", $this->name, $this->id);
+		
+		if($stmt->execute()){
+			return true;
+		}
+	 
+		return false;
+	}
+	
+	function delete(){
+		
+		$stmt = $this->conn->prepare("
+			DELETE FROM ".$this->itemsTable." 
+			WHERE id = ?");
+			
+		$this->id = htmlspecialchars(strip_tags($this->id));
+	 
+		$stmt->bind_param("i", $this->id);
+	 
+		if($stmt->execute()){
+			return true;
+		}
+	 
+		return false;		 
+	}
+}
+?>
