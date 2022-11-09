f37b0ddce781a203fa0187952924054f78fa395a
Omar Soto <omarsotosainz@gmail.com>
2022-11-08 17:18:30 -0700

    Feat: Endpoint de validacion de usuario terminado

--- backend/class/Login.php
+++ backend/class/Login.php
@@ -14,10 +14,26 @@ class Login{
         $stmt = $this->conn->prepare("SELECT * FROM ".$this->itemsTable." WHERE usuario = ?");
         $stmt->bind_param("s", $this->usuario);			
 		$stmt->execute();			
-		$result = $stmt->get_result();		
-		return $result;	
+		$result = $stmt->get_result();
+
+        $item = $result->fetch_assoc();
+        extract($item); 
+
+        // return $result;
+        if($contrasena == md5($this->contrasena)){
+            $itemDetails=array(
+                "usuario_id" => $usuario_id,
+                "nombre" => $nombre,	
+                "usuario" => $usuario
+            ); 	
+		    return $itemDetails;	
+        }
+        else{
+            $itemDetails=array(
+                "usuario_id" => 0,
+            ); 
+            return $itemDetails;
+        }
 	}
-		
-	
 }
 ?>
--- backend/login/auth.php
+++ backend/login/auth.php
@@ -10,28 +10,20 @@ $db = $database->getConnection();
  
 $items = new Login($db);
 
-$items->usuario = (isset($_POST['usuario']) && $_POST['usuario']) ? $_POST['usuario'] : 'x';
-$items->contrasena = (isset($_POST['contrasena']) && $_POST['contrasena']) ? $_POST['contrasena'] : 'x';
+$items->usuario = (isset($_POST['usuario'])) ? $_POST['usuario'] : 'x';
+$items->contrasena = (isset($_POST['contrasena'])) ? $_POST['contrasena'] : 'x';
 
 $result = $items->auth();
 
-if($result->num_rows > 0){    
+if($result['usuario_id'] > 0){
     $itemRecords=array();
     $itemRecords["items"]=array(); 
-	while ($item = $result->fetch_assoc()) { 	
-        extract($item); 
-        $itemDetails=array(
-            "usuario_id" => $usuario_id,
-            "nombre" => $nombre,	
-            "usuario" => $usuario
-        ); 
-       array_push($itemRecords["items"], $itemDetails);
-    }    
-    http_response_code(200);     
+    array_push($itemRecords["items"], $result); 
+    http_response_code(200); 
     echo json_encode($itemRecords);
 }else{     
     http_response_code(404);     
     echo json_encode(
-        array("message" => "Incomplete user data: " . $items->usuario)
+        array("message" => "Invalid user data." )
     );
 } 
